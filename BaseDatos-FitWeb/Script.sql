ef840f2941a09153611408e1c6e18afeed490904
Soy Cristian <cristianestrada0918@gmail.com>
2024-04-02 12:04:19 -0500

    Agregue el script BD

---
+++ BaseDatos-FitWeb/Script.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS FITWEB;
+CREATE DATABASE FITWEB;
+USE FITWEB;
+
+CREATE TABLE Persona (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombreCompleto VARCHAR(50),
+    numeroIdentificacion VARCHAR(20),
+    fechaNacimiento DATE,
+    edad INT,
+    correo   VARCHAR(50),
+    telefono VARCHAR(15),
+    sexo     VARCHAR(15),
+    contraseña VARCHAR(50)
+);
+
+CREATE TABLE Entrenador (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    persona_id INT,
+    especialidad VARCHAR(50),
+    FOREIGN KEY (persona_id) REFERENCES Persona(id)
+);
+
+CREATE TABLE Miembro (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    persona_id INT,
+    estado BOOLEAN,
+    FOREIGN KEY (persona_id) REFERENCES Persona(id)
+);
+
+CREATE TABLE MembresiaPagar (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numeroIdentificacion VARCHAR NOT NULL,
+    fecha_inicio DATE,
+    plan VARCHAR(25),
+    fecha_fin DATE,
+    tarjeta INT NOT NULL
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
