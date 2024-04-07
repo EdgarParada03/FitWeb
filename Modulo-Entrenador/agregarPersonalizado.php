bfb37e614712f7651e70b152b83cfd0ffd42d78e
jose sandoval <luisvalr120@gmail.com>
2024-04-06 19:40:18 -0500

    agregue videos en formato emb

--- Modulo-Entrenador/agregarPersonalizado.php
+++ Modulo-Entrenador/agregarPersonalizado.php
@@ -1,8 +1,19 @@
 <?php
 include 'conexion.php';
 
+function convertir_a_iframe_embebido($url) {
+    // Reemplaza la URL base con la URL de YouTube
+    $url_base = 'https://www.youtube.com/watch?v=';
+    $video_id = substr($url, strlen($url_base));
+ 
+    // Genera el código del iframe embebido
+    $iframe_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $video_id . '" frameborder="0" allowfullscreen></iframe>';
+ 
+    return $iframe_code;
+}
+
 // Recibir datos del formulario
-$video = $_POST['video'];
+$video = convertir_a_iframe_embebido($_POST['video']);
 $plan_alimentacion = $_POST['plan_alimentacion'];
 $documento = $_POST['documento'];
 
--- Modulo-Entrenador/agregarRutina.php
+++ Modulo-Entrenador/agregarRutina.php
@@ -1,13 +1,27 @@
 <?php
 include 'conexion.php';
 
+
+function convertir_a_iframe_embebido($url) {
+    // Reemplaza la URL base con la URL de YouTube
+    $url_base = 'https://www.youtube.com/watch?v=';
+    $video_id = substr($url, strlen($url_base));
+ 
+    // Genera el código del iframe embebido
+    $iframe_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $video_id . '" frameborder="0" allowfullscreen></iframe>';
+ 
+    return $iframe_code;
+}
+
+
+
 // Recibir datos del formulario
 $nombre = $_POST['nombre'];
 $zonaMuscular = $_POST['zonaMuscular'];
 $ejercicios = $_POST['ejercicios'];
 $series = $_POST['series'];
 $descripcion = $_POST['descripcion'];
-$video = $_POST['video'];
+$video = convertir_a_iframe_embebido($_POST['video']);
 
 // Insertar datos en la tabla
 $sql = "INSERT INTO Rutinas (nombre, zona_muscular, ejercicios, series, descripcion, video) VALUES ('$nombre', '$zonaMuscular ', '$ejercicios', '$series ', '$descripcion', '$video')";
