162adc8e737a86a7ca7708402f0ebf2d827b48d8
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2024-04-06 20:23:39 -0500

    YA CARGA EL VIDEO BIEN!!!!!

--- Modulo-Entrenador/agregarPersonalizado.php
+++ Modulo-Entrenador/agregarPersonalizado.php
@@ -1,29 +1,17 @@
 <?php
 include 'conexion.php';
 
-// Incluir la función convertir_a_iframe_embebido
-function convertir_a_iframe_embebido($url) {
-    // Reemplaza la URL base con la URL de YouTube
-    $url_base = 'https://www.youtube.com/watch?v=';
-    $video_id = substr($url, strlen($url_base));
 
-    // Genera el código del iframe embebido
-    $iframe_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $video_id . '" frameborder="0" allowfullscreen></iframe>';
-
-    return $iframe_code;
-}
 
 // Recibir datos del formulario
 $video = $_POST['video'];
 $plan_alimentacion = $_POST['plan_alimentacion'];
 $documento = $_POST['documento'];
 
-$video_iframe = convertir_a_iframe_embebido($video);
-
 
 // Insertar datos en la tabla
 $sql = "INSERT INTO Personalizados (video, plan_alimentacion, documento) 
-VALUES ('$video_iframe', '$plan_alimentacion ', '$documento')";
+VALUES ('$video', '$plan_alimentacion', '$documento')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Registro guardado exitosamente";
--- Modulo-Entrenador/agregarRutina.php
+++ Modulo-Entrenador/agregarRutina.php
@@ -2,17 +2,7 @@
 include 'conexion.php';
 
 
-// Incluir la función convertir_a_iframe_embebido
-function convertir_a_iframe_embebido($url) {
-    // Reemplaza la URL base con la URL de YouTube
-    $url_base = 'https://www.youtube.com/watch?v=';
-    $video_id = substr($url, strlen($url_base));
 
-    // Genera el código del iframe embebido
-    $iframe_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $video_id . '" frameborder="0" allowfullscreen></iframe>';
-
-    return $iframe_code;
-}
 
 
 
@@ -24,10 +14,9 @@ $series = $_POST['series'];
 $descripcion = $_POST['descripcion'];
 $video = $_POST['video'];
 
-$video_iframe = convertir_a_iframe_embebido($video);
 
 // Insertar datos en la tabla
-$sql = "INSERT INTO Rutinas (nombre, zona_muscular, ejercicios, series, descripcion, video) VALUES ('$nombre', '$zonaMuscular ', '$ejercicios', '$series ', '$descripcion', '$video_iframe')";
+$sql = "INSERT INTO Rutinas (nombre, zona_muscular, ejercicios, series, descripcion, video) VALUES ('$nombre', '$zonaMuscular ', '$ejercicios', '$series ', '$descripcion', '$video')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Registro guardado exitosamente";
