932e788bbdc259da1209f2ea5808a37bb75ba6d0
jose sandoval <luisvalr120@gmail.com>
2024-04-06 19:55:30 -0500

    formato embeb ahora si jaja

--- Modulo-Entrenador/agregarRutina.php
+++ Modulo-Entrenador/agregarRutina.php
@@ -2,14 +2,15 @@
 include 'conexion.php';
 
 
+// Incluir la función convertir_a_iframe_embebido
 function convertir_a_iframe_embebido($url) {
     // Reemplaza la URL base con la URL de YouTube
     $url_base = 'https://www.youtube.com/watch?v=';
     $video_id = substr($url, strlen($url_base));
- 
+
     // Genera el código del iframe embebido
     $iframe_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $video_id . '" frameborder="0" allowfullscreen></iframe>';
- 
+
     return $iframe_code;
 }
 
@@ -21,10 +22,12 @@ $zonaMuscular = $_POST['zonaMuscular'];
 $ejercicios = $_POST['ejercicios'];
 $series = $_POST['series'];
 $descripcion = $_POST['descripcion'];
-$video = convertir_a_iframe_embebido($_POST['video']);
+$video = $_POST['video'];
+
+$video_iframe = convertir_a_iframe_embebido($video);
 
 // Insertar datos en la tabla
-$sql = "INSERT INTO Rutinas (nombre, zona_muscular, ejercicios, series, descripcion, video) VALUES ('$nombre', '$zonaMuscular ', '$ejercicios', '$series ', '$descripcion', '$video')";
+$sql = "INSERT INTO Rutinas (nombre, zona_muscular, ejercicios, series, descripcion, video) VALUES ('$nombre', '$zonaMuscular ', '$ejercicios', '$series ', '$descripcion', '$video_iframe')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Registro guardado exitosamente";
